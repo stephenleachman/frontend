e7b0f2f10f0218a8d7160a155870327c56d5ec8f
Stephen Leachman <globalumbrella16@gmail.com>
2023-12-27 21:57:16 -0500

    update

--- src/app/(services)/services/page.tsx
+++ src/app/(services)/services/page.tsx
@@ -3,7 +3,7 @@ export default function page() {
   return (
     <div className="bg-custom-gray dark:bg-custom-dark-4 h-screen  grid place-content-center px-4 md:px-10">
       <div className="flex justify-center ">
-        <div className="container mt-[-50px]">
+        <div className="container pb-[300px]">
           <h1 className="text-5xl md:text-7xl tracking-wide bg-gradient-to-r from-custom-blue to-custom-green bg-clip-text text-transparent pb-2 text-center">My Services</h1>
           <p className="text-custom-dark-1 dark:text-custom-dark-text leading-relaxed text-lg mt-5 md:mt-10 lg:max-w-[700px] text-center">
             This page is currently in development. Stay tuned estimated lunch date, February 2024
