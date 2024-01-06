bfe3a422625ca2dc8a67961526df5c53933c95c0
Stephen Leachman <globalumbrella16@gmail.com>
2024-01-06 15:29:54 -0500

    update

--- src/app/(landing)/_sections/FeturedPosts.tsx
+++ src/app/(landing)/_sections/FeturedPosts.tsx
@@ -33,19 +33,29 @@ async function FeaturedPosts() {
           <div className="container grid z-10 py-16 md:py-24">
             <h2 className="text-center text-5xl	dark:text-gray-50 text-custom-dark-2 tracking-wide">My Blog</h2>
             <div className="h-1 w-20 bg-gradient-to-r from-custom-blue to-custom-green rounded justify-self-center my-8"></div>
-            <div className="flex flex-col sm:flex-row sm:justify-between">
+            <div className="flex flex-col sm:flex-row sm:justify-between mb-5">
               <p className="">
                 My Featured Articles
               </p>
+              <div className="hidden sm:block">
+                <Link
+                  href="/blog"
+                  className="text-custom-dark-2 dark:text-gray-50 tracking-wide hover:opacity-75 transition-opacity font-semibold mb-5 mt-5 sm:mt-0 inline-block"
+                >
+                  View All Articles
+                </Link>
+              </div>
+            </div>
+            <div className="grid grid-col md:grid-cols-2 xl:grid-cols-3 gap-8">
+              {Posts && Posts.data.map((post: any) => ( <BlogCard key={post.id} post={post}/>))}
+            </div>
+            <div className="block sm:hidden">
               <Link
                 href="/blog"
                 className="text-custom-dark-2 dark:text-gray-50 tracking-wide hover:opacity-75 transition-opacity font-semibold mb-5 mt-5 sm:mt-0 inline-block"
               >
                 View All Articles
               </Link>
-            </div>
-            <div className="grid grid-col md:grid-cols-2 xl:grid-cols-3 gap-8">
-              {Posts && Posts.data.map((post: any) => ( <BlogCard key={post.id} post={post}/>))}
             </div>
           </div>
         </div>
--- src/app/(landing)/_sections/RecentProjects.tsx
+++ src/app/(landing)/_sections/RecentProjects.tsx
@@ -33,10 +33,11 @@ async function RecentProjects() {
         <div className="container grid z-10 py-16 md:py-24">
           <h2 className="text-center text-5xl	dark:text-gray-50 text-custom-dark-2 tracking-wide">My Projects</h2>
           <div className="h-1 w-20 bg-gradient-to-r from-custom-blue to-custom-green rounded justify-self-center my-8"></div>
-          <div className="flex flex-col sm:flex-row sm:justify-between">
-              <p className="">
-                My Latest Projects
-              </p>
+          <div className="flex flex-col sm:flex-row sm:justify-between mb-5">
+            <p className="">
+              My Latest Projects
+            </p>
+            <div className="hidden sm:block">
               <Link
                 href="/projects"
                 className="text-custom-dark-2 dark:text-gray-50 tracking-wide hover:opacity-75 transition-opacity font-semibold mb-5 mt-5 sm:mt-0 inline-block"
@@ -44,8 +45,17 @@ async function RecentProjects() {
                 View All Projects
               </Link>
             </div>
+          </div>
           <div className="grid grid-col md:grid-cols-2 xl:grid-cols-3 gap-8">
             {Projects && Projects.data.map((project: any) => ( <ProjectCard key={project.id} project={project}/>))}
+          </div>
+          <div className="sm:hidden">
+            <Link
+              href="/projects"
+              className="text-custom-dark-2 dark:text-gray-50 tracking-wide hover:opacity-75 transition-opacity font-semibold mt-5 inline-block"
+            >
+              View All Projects
+            </Link>
           </div>
         </div>
       </div>
