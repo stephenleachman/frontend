1da3fcef030ad99359f1237225abf8e6135ee8f1
Stephen Leachman <globalumbrella16@gmail.com>
2024-10-17 17:52:52 -0400

    ubdate

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -23,12 +23,12 @@ export default function Home() {
         <section id="work" className="px-4 md:px-10 bg-custom-gray dark:bg-custom-dark-4 relative border-b-[3px] border-white dark:border-custom-dark-1">
           <Work />
         </section>
-        <section id="projects" className="px-4 md:px-10 bg-custom-gray-1 dark:bg-custom-dark-4 relative border-b-[3px] border-white dark:border-custom-dark-1">
+        {/* <section id="projects" className="px-4 md:px-10 bg-custom-gray-1 dark:bg-custom-dark-4 relative border-b-[3px] border-white dark:border-custom-dark-1">
           <RecentProjects />
         </section>
         <section id="blog" className="px-4 md:px-10 bg-custom-gray dark:bg-custom-dark-4 relative border-b-[3px] border-white dark:border-custom-dark-1">
           <FeaturedPosts />
-        </section>
+        </section> */}
         <ContactSection />
       </main>
   )
