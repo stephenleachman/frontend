bded6452c6261a3f5c0996e69f0c6b04e970cd62
Stephen Leachman <globalumbrella16@gmail.com>
2024-10-18 15:12:52 -0400

    update

--- src/app/(landing)/_sections/HeroMobile.tsx
+++ src/app/(landing)/_sections/HeroMobile.tsx
@@ -12,52 +12,52 @@ import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa6";
 
 export default function HeroMobile() {
   return (
-    <div className="" >
-    <div className="flex justify-center h-2/5	 bg-custom-gray dark:bg-custom-dark-2 w-full border-b-[3px] border-white dark:border-custom-dark-1">
-      <div className=" container relative flex justify-center">
-        <div className="self-end h-400px">
-        <Image
-            src={HeroImageMobile2}
-            alt="hero image of Stephen Leachman"
-            height={400}
-            width={400}
-            priority
-            className="object-cover w-[280px] "
-          >
-          </Image>
-          </div>
-          <div className="absolute top-0 left-0 sm:hidden">
-            <Image
-             src={Logo}
-             alt="hero Logo"
-             height={80}
-             width={80}
-             priority
+    <div className="h-screen flex flex-col" >
+      <div className="flex justify-center 	 bg-custom-gray dark:bg-custom-dark-2 w-full border-b-[3px] border-white dark:border-custom-dark-1">
+        <div className=" container relative flex justify-center">
+          <div className="self-end h-400px">
+          <Image
+              src={HeroImageMobile2}
+              alt="hero image of Stephen Leachman"
+              height={400}
+              width={400}
+              priority
+              className="object-cover w-[280px] "
             >
             </Image>
-          </div>
-          <div className="absolute top-4 right-5 lg:hidden">
-            <ThemeToggler />
-          </div>
-          <div className="flex flex-col justify-center sm:justify-end gap-8 absolute left-5 bottom-5 sm:hidden">
-          {socialLinks.map((item) => (
-            <Link 
-              key={item.title}
-              href={item.url}
-              target="_blank"
-            >
-            <div className="py-2 text-xs grid justify-items-center text-custom-gray-link dark:text-custom-dark-text hover:text-sky-500 transition dark:hover:text-sky-500 ease delay-100 self-center"> 
-              {item.icon === 'FaLinkedin' && <FaLinkedin className="text-2xl"/>}
-              {item.icon === 'FaInstagram' && <FaInstagram className="text-2xl"/>}
-              {item.icon === 'FaGithub' && <FaGithub className="text-2xl"/>}
             </div>
-            </Link>
-          ))}
+            <div className="absolute top-0 left-0 sm:hidden">
+              <Image
+              src={Logo}
+              alt="hero Logo"
+              height={80}
+              width={80}
+              priority
+              >
+              </Image>
+            </div>
+            <div className="absolute top-4 right-5 lg:hidden">
+              <ThemeToggler />
+            </div>
+            <div className="flex flex-col justify-center sm:justify-end gap-8 absolute left-5 bottom-5 sm:hidden">
+            {socialLinks.map((item) => (
+              <Link 
+                key={item.title}
+                href={item.url}
+                target="_blank"
+              >
+              <div className="py-2 text-xs grid justify-items-center text-custom-gray-link dark:text-custom-dark-text hover:text-sky-500 transition dark:hover:text-sky-500 ease delay-100 self-center"> 
+                {item.icon === 'FaLinkedin' && <FaLinkedin className="text-2xl"/>}
+                {item.icon === 'FaInstagram' && <FaInstagram className="text-2xl"/>}
+                {item.icon === 'FaGithub' && <FaGithub className="text-2xl"/>}
+              </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-     <div className="flex justify-center h-3/5 dark:bg-custom-dark-4 w-full border-b-[3px] border-white dark:border-custom-dark-1 relative pb-10">
-     <div className="bg-gradient-to-r from-custom-Opacity-blue to-custom-Opacity-green absolute h-full w-full z-0 dark:hidden"></div>
+     <div className="flex-grow dark:bg-custom-dark-4 w-full border-b-[3px] border-white dark:border-custom-dark-1 relative pb-10">
+      <div className="bg-gradient-to-r from-custom-Opacity-blue to-custom-Opacity-green absolute h-full w-full z-0 dark:hidden"></div>
         <div className="container px-4 md:px-10 z-10 ">
           <div className="flex items-center mt-10">
             <h2 className="text-custom-dark-2 dark:text-custom-dark-text text-3xl mb-3 tracking-wide font-semibold	">👋 Hey, I’m</h2>
