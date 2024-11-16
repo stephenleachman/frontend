24660d7482f7f425030d646dd18bce077a717f82
Stephen Leachman <globalumbrella16@gmail.com>
2024-11-16 18:15:13 -0500

    update

--- src/app/(projects)/_components/ProjectPageDetails.tsx
+++ src/app/(projects)/_components/ProjectPageDetails.tsx
@@ -10,7 +10,7 @@ export default function ProjectPageDetails(project: any) {
 
  
   return (
-    <div id="card" className="sm:rounded-xl max-w-full bg-background-card-2 sm:shadow sm:ring-1 ring-ring-color-1 p-5 pb-5 sm:py-5" >
+    <div id="card" className="sm:rounded-xl max-w-full bg-background-card-2 sm:shadow sm:ring-1 ring-ring-color-1 p-5 pb-5 sm:py-5 overflow-hidden" >
       <h2 className="text-4xl font-medium mt-5 tracking-wider bg-gradient-to-r from-p-text to-hedding-text bg-clip-text text-transparent pb-1 mb-[-30px]">
         About This Project
       </h2>
